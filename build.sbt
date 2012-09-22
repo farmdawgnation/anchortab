79ec043f94a7a983a59251d0a917d6d89e43e85e
Matt Farmer <matt@frmr.me>
2012-09-22 12:17:53 -0400

    Import of the basic Lift 2.5 project, with MongoDB modifications.

---
+++ build.sbt
@@ -0,0 +1,27 @@
+name := "Anchor Tab"
+
+version := "0.1.0"
+
+organization := "com.anchortab"
+
+scalaVersion := "2.9.2"
+
+resolvers ++= Seq("snapshots"     at "http://oss.sonatype.org/content/repositories/snapshots",
+                "releases"        at "http://oss.sonatype.org/content/repositories/releases"
+                )
+
+seq(com.github.siasia.WebPlugin.webSettings :_*)
+
+scalacOptions ++= Seq("-deprecation", "-unchecked")
+
+libraryDependencies ++= {
+  val liftVersion = "2.5-M1"
+  Seq(
+    "net.liftweb"       %% "lift-webkit"        % liftVersion        % "compile",
+    "net.liftweb"       %% "lift-mongodb"       % liftVersion        % "compile",
+    "net.liftmodules"   %% "lift-jquery-module" % (liftVersion + "-1.0"),
+    "org.eclipse.jetty" % "jetty-webapp"        % "8.1.7.v20120910"  % "container",
+    "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container" artifacts Artifact("javax.servlet", "jar", "jar"),
+    "ch.qos.logback"    % "logback-classic"     % "1.0.6"
+  )
+}
---
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -0,0 +1,47 @@
+package bootstrap.liftweb
+
+import net.liftweb._
+  import util._
+    import Helpers._
+  import common._
+  import http._
+  import js.jquery.JQueryArtifacts
+  import sitemap._
+  import Loc._
+  import mongodb._
+
+import net.liftmodules.JQueryModule
+
+
+/**
+ * A class that's instantiated early and run.  It allows the application
+ * to modify lift's environment
+ */
+class Boot {
+  def boot {
+    // Connect to MongoDB
+    for {
+      hostname <- Props.get("mongodb.host")
+      port <- Props.get("mongodb.port").map(_.toInt)
+      database <- Props.get("mongodb.database")
+    } {
+      val mongoHost = MongoHost(hostname, port)
+      MongoDB.defineDb(DefaultMongoIdentifier, MongoAddress(mongoHost, database))
+    }
+
+    // Specify where to search for our stuff.
+    LiftRules.addToPackages("com.anchortab")
+
+    //Init the jQuery module, see http://liftweb.net/jquery for more information.
+    LiftRules.jsArtifacts = JQueryArtifacts
+    JQueryModule.InitParam.JQuery=JQueryModule.JQuery172
+    JQueryModule.init()
+
+    // Force the request to be UTF-8
+    LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
+
+    // Use HTML5 for rendering
+    LiftRules.htmlProperties.default.set((r: Req) =>
+      new Html5Properties(r.userAgent))
+  }
+}
