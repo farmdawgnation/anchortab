93bffca5ad013e1faa0be82107ac1597b60da28b
Matt Farmer <matt@frmr.me>
2012-09-24 23:24:38 -0400

    Updates to the user model.

--- src/main/scala/com/anchortab/model/User.scala
+++ src/main/scala/com/anchortab/model/User.scala
@@ -2,6 +2,7 @@ package com.anchortab.model
 
 import net.liftweb._
   import mongodb._
+  import util.Helpers._
 
 import org.joda.time.DateTime
 
@@ -15,11 +16,14 @@ case class UserSession(userId:ObjectId, ip:String, userAgent:String,
 object UserSession extends MongoDocumentMeta[UserSession] {
 }
 
+case class UserAuthorizationKey(appName:String, key:String = randomString(32),
+                                createdAt:DateTime = new DateTime)
+
 case class UserProfile(firstName:Option[String], lastName:Option[String], organization:Option[String])
 
 case class User(email:String, password:String, profile:Option[UserProfile] = None,
-                createdAt:DateTime = new DateTime,
-                _id:ObjectId = ObjectId.get) extends MongoDocument[User] {
+                authorizations:List[UserAuthorizationKey] = List(), role:Option[String] = None,
+                createdAt:DateTime = new DateTime, _id:ObjectId = ObjectId.get) extends MongoDocument[User] {
   val meta = User
 }
 object User extends MongoDocumentMeta[User] {
