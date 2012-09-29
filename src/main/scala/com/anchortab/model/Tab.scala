a7f2495258fcfe582760292be9613af59b79e1e8
Matt Farmer <matt@frmr.me>
2012-09-29 13:13:32 -0400

    Add helper to render a Tab as JSON.

--- src/main/scala/com/anchortab/model/Tab.scala
+++ src/main/scala/com/anchortab/model/Tab.scala
@@ -3,6 +3,9 @@ package com.anchortab.model
 import net.liftweb._
   import common._
   import mongodb._
+  import json._
+    import JsonDSL._
+    import Extraction._
 
 import org.bson.types.ObjectId
 
@@ -13,8 +16,14 @@ case class TabService(serviceId:String, credentials:Map[String,String])
 case class Tab(name:String, userId:ObjectId, appearance:TabAppearance, service:Option[TabService] = None,
                _id:ObjectId = ObjectId.get) extends MongoDocument[Tab] {
   val meta = Tab
+  implicit val formats = DefaultFormats
 
   lazy val user : Box[User] = User.find(userId)
+
+  lazy val asJson =
+    ("name" -> name) ~
+    ("appearance" -> decompose(appearance)) ~
+    ("service" -> decompose(service))
 }
 
 object Tab extends MongoDocumentMeta[Tab]
