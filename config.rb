76fa7b8cb47d96376641604a812e071c0bc95d4d
Matt Farmer <matt@frmr.me>
2012-10-14 16:00:24 -0400

    Add initial tab stylesheet.

---
+++ config.rb
@@ -0,0 +1,12 @@
+# Compass configuration
+#http_path = (environment == :production) ? "http://local.openstudy.com/" : "/"
+project_path = "src/main/webapp"
+css_dir = "stylesheets"
+sass_dir = "sass-hidden"
+images_dir = "images"
+javascripts_dir = "javascripts"
+fonts_dir = "stylesheets/fonts"
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = environment != :production
+# output_style = (environment == :production) ? :compressed : :expanded
