4b0ee99985d154252d0278780500b96f0f19dd19
Matt Farmer <matt@frmr.me>
2012-10-18 02:26:51 -0400

    Implement Event tracking models.

---
+++ src/main/scala/com/anchortab/model/Event.scala
@@ -0,0 +1,58 @@
+package com.anchortab.model
+
+import net.liftweb._
+  import common._
+  import mongodb._
+  import util.Helpers._
+  import json._
+    import ext._
+    import JsonDSL._
+    import Extraction._
+
+import org.joda.time.DateTime
+
+import org.bson.types.ObjectId
+
+case class UniqueEventActor(ip:String, userAgent:String, tabId:ObjectId,
+                            cookieId:String = randomString(32),
+                            createdAt:DateTime = new DateTime,
+                            _id:ObjectId = ObjectId.get)
+    extends MongoDocument[UniqueEventActor] {
+  val meta = UniqueEventActor
+}
+object UniqueEventActor extends MongoDocumentMeta[UniqueEventActor] {
+  override def formats = allFormats ++ JodaTimeSerializers.all
+}
+
+case class Event(eventType:String, ip:String, userAgent:String, userId:ObjectId,
+                 tabId:ObjectId, email:Option[String] = None,
+                 uniqueEventActorId:ObjectId,
+                 createdAt:DateTime = new DateTime, _id:ObjectId = ObjectId.get)
+    extends MongoDocument[Event] {
+  val meta = Event
+}
+object Event extends MongoDocumentMeta[Event] {
+  override def formats = allFormats ++ JodaTimeSerializers.all
+
+  object Types {
+    val TabView = "tab-view"
+    val TabSubmit = "tab-submit"
+    val TabClose = "tab-close"
+    val TabReopen = "tab-reopen"
+  }
+}
+
+object EventLog {
+  def track(eventType:String, ip:String, userAgent:String, userId:ObjectId, tabId:ObjectId, cookieId:Option[String], email:Option[String] = None) {
+    val uniqueEventActor =
+      UniqueEventActor.find("cookieId" -> cookieId) match {
+        case Some(actor:UniqueEventActor) => actor
+        case _ =>
+          val actor = UniqueEventActor(ip, userAgent, tabId)
+          actor.save
+          actor
+      }
+
+    Event(eventType, ip, userAgent, userId, tabId, email, uniqueEventActor._id).save
+  }
+}
