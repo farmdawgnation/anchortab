5b4cf8c5acf4887c24b90f99bba9311758178690
Matt Farmer <matt@frmr.me>
2012-10-22 13:09:21 -0400

    Add placeholder EmailActor.

---
+++ src/main/scala/com/anchortab/actor/EmailActor.scala
@@ -0,0 +1,19 @@
+package com.anchortab.actor
+
+import net.liftweb._
+  import common._
+  import actor._
+  import mongodb._
+  import util.Helpers._
+  import json._
+    import ext._
+    import JsonDSL._
+    import Extraction._
+
+import org.bson.types.ObjectId
+
+object EmailActor extends LiftActor {
+  def messageHandler = {
+    // TODO
+  }
+}
