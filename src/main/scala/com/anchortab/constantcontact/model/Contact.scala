07e5f46383682688aaddb6f8d10db21031c6d633
Matt Farmer <matt@frmr.me>
2012-12-24 21:01:03 -0500

    Serialization like whoa.

--- src/main/scala/com/anchortab/constantcontact/model/Contact.scala
+++ src/main/scala/com/anchortab/constantcontact/model/Contact.scala
@@ -9,7 +9,7 @@ import net.liftweb._
 import org.joda.time._
 
 object Contact {
-  val formats = DefaultFormats ++ List(
+  val formats = List(
     ActionBySerializer,
     AddressTypeSerializer,
     ContactListStatusSerializer,
@@ -18,31 +18,87 @@ object Contact {
     ContactSerializer
   )
 
+  val nonContactFormats = List(
+    ActionBySerializer,
+    AddressTypeSerializer,
+    ContactListStatusSerializer,
+    EmailConfirmStatusSerializer,
+    StatusSerializer
+  )
+
   object ActionBy extends Enumeration {
     val Visitor = Value("ACTION_BY_VISITOR")
     val Owner = Value("ACTION_BY_OWNER")
   }
-  object ActionBySerializer extends EnumerationSerializer(ActionBy)
+  object ActionBySerializer extends Serializer[ActionBy.Value] {
+    private val Class = classOf[ActionBy.Value]
+
+    def deserialize(implicit format: Formats): PartialFunction[(TypeInfo, JValue), ActionBy.Value] = {
+      case (TypeInfo(Class, _), json) =>
+        val stringValue = json.extract[String]
+        ActionBy.withName(stringValue)
+    }
+
+    def serialize(implicit format: Formats): PartialFunction[Any, JValue] = {
+      case x:ActionBy.Value => x.toString
+    }
+  }
 
   object AddressType extends Enumeration {
     val Business = Value("business")
     val Personal = Value("personal")
     val Unknown = Value("unknown")
   }
-  object AddressTypeSerializer extends EnumerationSerializer(AddressType)
+  object AddressTypeSerializer extends Serializer[AddressType.Value] {
+    private val Class = classOf[AddressType.Value]
+
+    def deserialize(implicit format: Formats): PartialFunction[(TypeInfo, JValue), AddressType.Value] = {
+      case (TypeInfo(Class, _), json) =>
+        val stringValue = json.extract[String]
+        AddressType.withName(stringValue)
+    }
+
+    def serialize(implicit format: Formats): PartialFunction[Any, JValue] = {
+      case x:AddressType.Value => x.toString
+    }
+  }
 
   object ContactListStatus extends Enumeration {
     val Active = Value("ACTIVE")
     val Hidden = Value("HIDDEN")
   }
-  object ContactListStatusSerializer extends EnumerationSerializer(ContactListStatus)
+  object ContactListStatusSerializer extends Serializer[ContactListStatus.Value] {
+    private val Class = classOf[ContactListStatus.Value]
+
+    def deserialize(implicit format: Formats): PartialFunction[(TypeInfo, JValue), ContactListStatus.Value] = {
+      case (TypeInfo(Class, _), json) =>
+        val stringValue = json.extract[String]
+        ContactListStatus.withName(stringValue)
+    }
+
+    def serialize(implicit format: Formats): PartialFunction[Any, JValue] = {
+      case x:ContactListStatus.Value => x.toString
+    }
+  }
 
   object EmailConfirmStatus extends Enumeration {
     val Confirmed = Value("CONFIRMED")
     val NoConfirmationRequired = Value("NO_CONFIRMATION_REQUIRED")
     val Unconfirmed = Value("UNCONFIRMED")
   }
-  object EmailConfirmStatusSerializer extends EnumerationSerializer(EmailConfirmStatus)
+  object EmailConfirmStatusSerializer extends Serializer[EmailConfirmStatus.Value] {
+    private val Class = classOf[EmailConfirmStatus.Value]
+
+    def deserialize(implicit format: Formats): PartialFunction[(TypeInfo, JValue), EmailConfirmStatus.Value] = {
+      case (TypeInfo(Class, _), json) =>
+        val stringValue = json.extract[String]
+        EmailConfirmStatus.withName(stringValue)
+    }
+
+    def serialize(implicit format: Formats): PartialFunction[Any, JValue] = {
+      case x:EmailConfirmStatus.Value => x.toString
+    }
+  }
 
   object Status extends Enumeration {
     val Active = Value("ACTIVE")
@@ -52,7 +108,19 @@ object Contact {
     val NonSubscriber = Value("NON_SUBSCRIBER")
     val Visitor = Value("VISITOR")
   }
-  object StatusSerializer extends EnumerationSerializer(Status)
+  object StatusSerializer extends Serializer[Status.Value] {
+    private val Class = classOf[Status.Value]
+
+    def deserialize(implicit format: Formats): PartialFunction[(TypeInfo, JValue), Status.Value] = {
+      case (TypeInfo(Class, _), json) =>
+        val stringValue = json.extract[String]
+        Status.withName(stringValue)
+    }
+
+    def serialize(implicit format: Formats): PartialFunction[Any, JValue] = {
+      case x:Status.Value => x.toString
+    }
+  }
 
   case class Address( address_type:String, line1:String, line2:String, state_code:String,
                       country_code:String, postal_code:String, sub_postal_code:String)
@@ -73,7 +141,7 @@ object Contact {
                             cell_phone:Option[String] = None)
   case class ContactName( first_name:Option[String] = None, middle_name:Option[String] = None,
                           last_name:Option[String] = None)
-  case class Contact(email_addresses:List[EmailAddress], action_by:String, id:Long = 0,
+  case class Contact(email_addresses:List[EmailAddress], action_by:ActionBy.Value, id:Long = 0,
                       status:Option[Status.Value] = None, prefix_name:Option[String] = None,
                       name:Option[ContactName] = None, job_title:Option[String] = None,
                       department_name:Option[String] = None, company_name:Option[String] = None,
@@ -90,23 +158,23 @@ object Contact {
 
     def deserialize(implicit format: Formats): PartialFunction[(TypeInfo, JValue), Contact] = {
       case (TypeInfo(Class, _), json) =>
-        implicit val formats = DefaultFormats
+        implicit val formats = DefaultFormats ++ nonContactFormats
 
         val contactName = {
-          val extractedName = json.extract[ContactName]
+          val extractedName = json.extract[ContactName](formats, manifest[ContactName])
           extractedName match {
             case ContactName(None, None, None) => None
             case _ => Some(extractedName)
           }
         }
         val contactPhone = {
-          val extractedPhone = json.extract[ContactPhones]
+          val extractedPhone = json.extract[ContactPhones](formats, manifest[ContactPhones])
           extractedPhone match {
             case ContactPhones(None, None, None) => None
             case _ => Some(extractedPhone)
           }
         }
-        val contact = json.extract[Contact]
+        val contact = json.extract[Contact](formats, manifest[Contact])
 
         contact.copy(name = contactName, phone = contactPhone)
     }
@@ -114,12 +182,12 @@ object Contact {
     def serialize(implicit format: Formats): PartialFunction[Any, JValue] = {
       case x:Contact =>
         // Decompose as a normal case class.
-        implicit val formats = DefaultFormats
-        var contactJson = decompose(x)
+        implicit val formats = DefaultFormats ++ nonContactFormats
+        var contactJson = decompose(x)(formats)
 
         // Extract names and phone numbers
-        val contactNameJson = x.name.map(decompose(_)) getOrElse JObject(Nil)
-        val contactPhoneJson = x.phone.map(decompose(_)) getOrElse JObject(Nil)
+        val contactNameJson = x.name.map(decompose(_)(formats)) getOrElse JObject(Nil)
+        val contactPhoneJson = x.phone.map(decompose(_)(formats)) getOrElse JObject(Nil)
 
         val filteredJvalue = contactJson.remove { contactfield =>
           contactfield match {
