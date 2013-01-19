32a945da2467565cf968da9242b6b56bbe8332ad
Matt Farmer <matt@frmr.me>
2013-01-19 18:40:42 -0500

    Ability to create Invite codes from the UI.

--- src/main/scala/com/anchortab/snippet/Invites.scala
+++ src/main/scala/com/anchortab/snippet/Invites.scala
@@ -18,11 +18,12 @@ import net.liftweb._
     import JsonDSL._
   import mongodb.BsonDSL._
 
-import com.anchortab.model.InviteCode
+import com.anchortab.model.{InviteCode, Plan}
 
 import org.bson.types.ObjectId
 
 object inviteCode extends RequestVar[Box[InviteCode]](Empty)
+object requestInviteId extends RequestVar[Box[ObjectId]](Empty)
 
 object Invites extends Loggable {
   def statelessRewrite: RewritePF = {
@@ -35,10 +36,76 @@ object Invites extends Loggable {
       }
 
       RewriteResponse("register" :: Nil)
+
+    case RewriteRequest(ParsePath("admin" :: "invites" :: "new" :: Nil, _, _, _), _, _) =>
+      RewriteResponse("admin" :: "invite" :: "form" :: Nil)
+
+    case RewriteRequest(ParsePath("admin" :: "invite" :: inviteId :: "edit" :: Nil, _, _, _), _, _) =>
+      {
+        for {
+          inviteObjectId <- tryo(new ObjectId(inviteId))
+        } yield {
+          requestInviteId(Full(inviteObjectId))
+
+          RewriteResponse("admin" :: "invite" :: "form" :: Nil)
+        }
+      } openOr {
+        S.redirectTo("/admin/invites")
+      }
   }
 
   def snippetHandlers: SnippetPF = {
     case "invites-list" :: Nil => invitesList
+    case "new-invite-button" :: Nil => newInviteButton
+    case "invite-form" :: Nil => inviteForm
+  }
+
+  def inviteForm = {
+    val requestInviteCode = requestInviteId.is.flatMap(InviteCode.find(_)) openOr InviteCode()
+
+    var theCode = requestInviteCode.code
+    var maxUses = requestInviteCode.numberOfUsesAvailable.map(_.toString) getOrElse ""
+    var validityStart = ""
+    var validityEnd = ""
+    var plan: Option[Plan] = None
+
+    def saveInviteCode() = {
+      val numUses = {
+        maxUses match {
+          case "" => None
+          case x => Some(x.toInt)
+        }
+      }
+
+      requestInviteCode.copy(
+        numberOfUsesAvailable = numUses,
+        forPlanId = plan.map(_._id)
+      ).save
+
+      RedirectTo("/admin/invites")
+    }
+
+    val bind =
+      ".current-code" #> text(theCode, theCode = _) &
+      ".max-uses" #> text(maxUses, maxUses = _) &
+      ".validity-start" #> text(validityStart, validityStart = _) &
+      ".valitiy-end" #> text(validityEnd, validityEnd = _) &
+      ".plan" #> selectObj[Option[Plan]](
+        List((None, "None")) ++ Plan.findAll.map(p => (Some(p), p.name)),
+        Empty,
+        plan = _
+      ) &
+      ".submit" #> ajaxSubmit("Save Invite Code", saveInviteCode _)
+
+    "form" #> { ns: NodeSeq =>
+      ajaxForm(
+        bind(ns)
+      )
+    }
+  }
+
+  def newInviteButton = {
+    ".new-invite [onclick]" #> onEvent((s: String) => RedirectTo("/admin/invites/new"))
   }
 
   def invitesList = {
