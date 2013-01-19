004d2c7b2f60c90dfc43a78dd5ae31486c0b313c
Matt Farmer <matt@frmr.me>
2013-01-19 10:07:38 -0500

    Implement InviteCode model.

---
+++ src/main/scala/com/anchortab/model/InviteCode.scala
@@ -0,0 +1,27 @@
+package com.anchortab.model
+
+import scala.collection.immutable.HashMap
+
+import net.liftweb._
+  import common._
+  import mongodb._
+  import util.Helpers._
+  import json._
+    import ext._
+    import JsonDSL._
+    import Extraction._
+  import util.Helpers._
+
+import org.joda.time.DateTime
+
+import org.bson.types.ObjectId
+
+case class InviteCode(numberOfUses: Int = 0, numberOfUsesAvailable: Option[Int] = None,
+                      validityStart: Option[DateTime] = None, validityEnd: Option[DateTime] = None,
+                      forPlan: Option[Plan] = None, code: String = randomString(32),
+                      _id:ObjectId = ObjectId.get) extends MongoDocument[InviteCode] {
+  val meta = InviteCode
+}
+object InviteCode extends MongoDocumentMeta[InviteCode] {
+  override def formats = allFormats ++ JodaTimeSerializers.all
+}
