3bb2c3465cb695cbd4947f9b5399470e77bb0b09
Matt Farmer <matt@frmr.me>
2013-04-02 20:15:44 -0400

    Refactor API processing for StripeHook into modular functions.

--- src/main/scala/com/anchortab/snippet/StripeHook.scala
+++ src/main/scala/com/anchortab/snippet/StripeHook.scala
@@ -53,6 +53,173 @@ object StripeHook extends RestHelper with Loggable {
     User.update("_id" -> user._id, "$addToSet" -> ("subscriptions" -> decompose(newSub)))
   }
 
+  def invoicePaymentSucceeded(objectJson: JValue) = {
+    for {
+      stripeCustomerId <- tryo((objectJson \ "customer").extract[String]) ?~! "No customer."
+      user <- User.find("stripeCustomerId" -> stripeCustomerId)
+      totalAmountInCents <- tryo((objectJson \ "total").extract[Long]) ?~! "No total."
+    } yield {
+      val amount = totalAmountInCents / 100d
+
+      EmailActor ! SendInvoicePaymentSucceededEmail(user.email, amount)
+      OkResponse()
+    }
+  }
+
+  def invoicePaymentFailed(objectJson: JValue) = {
+    for {
+      stripeCustomerId <- tryo((objectJson \ "customer").extract[String]) ?~! "No customer."
+      user <- User.find("stripeCustomerId" -> stripeCustomerId)
+      totalAmountInCents <- tryo((objectJson \ "total").extract[Long]) ?~! "No total."
+    } yield {
+      val nextPaymentAttemptSecs: Option[Long] =
+        tryo((objectJson \ "next_payment_attempt").extract[Option[Long]])
+        .flatten
+        .headOption
+
+      val nextPaymentAttempt = nextPaymentAttemptSecs.map { nextPaymentAttemptInSecs =>
+        new DateTime(nextPaymentAttemptInSecs * 1000)
+      } filter {
+        _ isAfterNow
+      }
+      val amount = totalAmountInCents / 100d
+
+      EmailActor ! SendInvoicePaymentFailedEmail(user.email, amount, nextPaymentAttempt)
+      OkResponse()
+    }
+  }
+
+  def customerSubscriptionCreated(objectJson: JValue) = {
+    // Create the subscription if it doesn't already exist for that user
+    // on our end.
+    for {
+      stripeCustomerId <- tryo((objectJson \ "customer").extract[String]) ?~! "No customer."
+      planJson = (objectJson \ "plan")
+      status <- (objectJson \ "status").extractOpt[String]
+      stripePlanId <- (planJson \ "id").extractOpt[String]
+      user <- User.find("stripeCustomerId" -> stripeCustomerId)
+      plan <- Plan.find("stripeId" -> stripePlanId)
+    } yield {
+      newPlanFromStripe(user, plan, status)
+
+      OkResponse()
+    }
+  }
+
+  def customerSubscriptionUpdated(objectJson: JValue) = {
+    // Update database to reflect new sub.
+    for {
+      stripeCustomerId <- (objectJson \ "customer").extractOpt[String]
+      planJson = (objectJson \ "plan")
+      user <- User.find("stripeCustomerId" -> stripeCustomerId)
+      currentUserSubscription <- user.subscription
+      status <- (objectJson \ "status").extractOpt[String]
+      stripePlanId <- (planJson \ "id").extractOpt[String]
+      cancelAtPeriodEnd <- (objectJson \ "cancel_at_period_end").extractOpt[Boolean]
+      plan <- Plan.find("stripeId" -> stripePlanId)
+    } yield {
+      implicit val formats = User.formats
+
+      if (plan._id == user.plan._id && currentUserSubscription.status != status) {
+        val planEnding = {
+          if (cancelAtPeriodEnd) {
+            val currentPeriodEnd = (objectJson \ "current_period_end").extractOpt[Long]
+            currentPeriodEnd.map(_ * 1000).map(new DateTime(_))
+          } else if (status == "canceled") {
+            val endedAt = (objectJson \ "ended_at").extractOpt[Long]
+            endedAt.map(_ * 1000).map(new DateTime(_))
+          } else {
+            None
+          }
+        }
+
+        val newStatus = status match {
+          case "trialing" => "trial"
+          case "canceled" => "cancelled"
+          case _ if cancelAtPeriodEnd => "cancelled"
+          case _ => "active"
+        }
+
+        User.update(
+          ("_id" -> user._id) ~
+          ("subscriptions._id" -> currentUserSubscription._id),
+          "$set" -> (
+            ("subscriptions.$.status" -> newStatus) ~
+            ("subscriptions.$.ends" -> decompose(planEnding))
+          )
+        )
+      } else if (plan._id != user.plan._id) {
+        newPlanFromStripe(user, plan, status)
+      }
+
+      OkResponse()
+    }
+  }
+
+  def customerSubscriptionDeleted(objectJson: JValue) = {
+    // Sorry to see you go email?
+    for {
+      stripeCustomerId <- (objectJson \ "customer").extractOpt[String]
+      user <- User.find("stripeCustomerId" -> stripeCustomerId)
+      status <- (objectJson \ "status").extractOpt[String]
+      periodEnd <- (objectJson \ "current_period_end").extractOpt[Long]
+    } yield {
+      implicit val formats = User.formats
+
+      user.subscription match {
+        case Some(subscription) =>
+          if (status == "canceled") {
+            User.update(
+              ("_id" -> user._id) ~
+              ("subscriptions._id" -> subscription._id),
+              "$set" -> (
+                ("subscriptions.$.status" -> "cancelled") ~
+                ("subscriptions.$.ends" -> decompose(new DateTime(periodEnd * 1000)))
+              )
+            )
+          } else { //unpaid
+            User.update(
+              ("_id" -> user._id) ~
+              ("subscriptions._id" -> subscription._id),
+              (
+                "$set" -> (
+                  ("subscriptions.$.status" -> "stopped") ~
+                  ("subscriptions.$.ends" -> decompose(new DateTime()))
+                ) ~
+                (
+                  "$unset" -> ("activeCard" -> true)
+                )
+              )
+            )
+          }
+
+        case _ =>
+      }
+
+      OkResponse()
+    }
+  }
+
+  def customerSubscriptionTrialWillEnd(objectJson: JValue) = {
+    for {
+      stripeCustomerId <- tryo((objectJson \ "customer").extract[String]) ?~! "No customer."
+      trialEndInSecs <- tryo((objectJson \ "trial_end").extract[Long]) ?~! "No trial end."
+      user <- User.find("stripeCustomerId" -> stripeCustomerId)
+      subscription <- user.subscription
+      plan <- subscription.plan
+    } yield {
+      val trialEnd = new DateTime(trialEndInSecs * 1000)
+
+      // We get a trial end event every time a user switches plans. So, we need to distinguish
+      // between those and the real ones that come in advance of a real trial ending. We use
+      // the heuristic of subtracting one hour from the time to see if it's still in the future.
+      if (trialEnd.minusHours(1) isAfterNow)
+        EmailActor ! SendTrialEndingEmail(user.email, user.activeCard.isDefined, plan.name, trialEnd)
+
+      OkResponse()
+    }
+  }
+
   serve {
     case req @ Req("stripe-hook" :: Nil, _, PostRequest) =>
       {
@@ -66,172 +233,13 @@ object StripeHook extends RestHelper with Loggable {
           println(pretty(render(requestJson)))
 
           val result: Box[LiftResponse] = eventType match {
-            case "invoice.payment_succeeded" =>
-              // Send an email reciept.
-              for {
-                stripeCustomerId <- tryo((objectJson \ "customer").extract[String]) ?~! "No customer."
-                user <- User.find("stripeCustomerId" -> stripeCustomerId)
-                totalAmountInCents <- tryo((objectJson \ "total").extract[Long]) ?~! "No total."
-              } yield {
-                val amount = totalAmountInCents / 100d
-
-                EmailActor ! SendInvoicePaymentSucceededEmail(user.email, amount)
-                OkResponse()
-              }
-
-            case "invoice.payment_failed" =>
-              for {
-                stripeCustomerId <- tryo((objectJson \ "customer").extract[String]) ?~! "No customer."
-                user <- User.find("stripeCustomerId" -> stripeCustomerId)
-                totalAmountInCents <- tryo((objectJson \ "total").extract[Long]) ?~! "No total."
-              } yield {
-                val nextPaymentAttemptSecs: Option[Long] =
-                  tryo((objectJson \ "next_payment_attempt").extract[Option[Long]])
-                  .flatten
-                  .headOption
-
-                val nextPaymentAttempt = nextPaymentAttemptSecs.map { nextPaymentAttemptInSecs =>
-                  new DateTime(nextPaymentAttemptInSecs * 1000)
-                } filter {
-                  _ isAfterNow
-                }
-                val amount = totalAmountInCents / 100d
-
-                EmailActor ! SendInvoicePaymentFailedEmail(user.email, amount, nextPaymentAttempt)
-                OkResponse()
-              }
-
-            case "customer.subscription.created" =>
-              // Create the subscription if it doesn't already exist for that user
-              // on our end.
-              for {
-                stripeCustomerId <- tryo((objectJson \ "customer").extract[String]) ?~! "No customer."
-                planJson = (objectJson \ "plan")
-                status <- (objectJson \ "status").extractOpt[String]
-                stripePlanId <- (planJson \ "id").extractOpt[String]
-                user <- User.find("stripeCustomerId" -> stripeCustomerId)
-                plan <- Plan.find("stripeId" -> stripePlanId)
-              } yield {
-                newPlanFromStripe(user, plan, status)
-
-                OkResponse()
-              }
-
-            case "customer.subscription.updated" =>
-              // Send updated alert email.
-
-              // Update database to reflect new sub.
-              for {
-                stripeCustomerId <- (objectJson \ "customer").extractOpt[String]
-                planJson = (objectJson \ "plan")
-                user <- User.find("stripeCustomerId" -> stripeCustomerId)
-                currentUserSubscription <- user.subscription
-                status <- (objectJson \ "status").extractOpt[String]
-                stripePlanId <- (planJson \ "id").extractOpt[String]
-                cancelAtPeriodEnd <- (objectJson \ "cancel_at_period_end").extractOpt[Boolean]
-                plan <- Plan.find("stripeId" -> stripePlanId)
-              } yield {
-                implicit val formats = User.formats
-
-                if (plan._id == user.plan._id && currentUserSubscription.status != status) {
-                  val planEnding = {
-                    if (cancelAtPeriodEnd) {
-                      val currentPeriodEnd = (objectJson \ "current_period_end").extractOpt[Long]
-                      currentPeriodEnd.map(_ * 1000).map(new DateTime(_))
-                    } else if (status == "canceled") {
-                      val endedAt = (objectJson \ "ended_at").extractOpt[Long]
-                      endedAt.map(_ * 1000).map(new DateTime(_))
-                    } else {
-                      None
-                    }
-                  }
-
-                  val newStatus = status match {
-                    case "trialing" => "trial"
-                    case "canceled" => "cancelled"
-                    case _ if cancelAtPeriodEnd => "cancelled"
-                    case _ => "active"
-                  }
-
-                  User.update(
-                    ("_id" -> user._id) ~
-                    ("subscriptions._id" -> currentUserSubscription._id),
-                    "$set" -> (
-                      ("subscriptions.$.status" -> newStatus) ~
-                      ("subscriptions.$.ends" -> decompose(planEnding))
-                    )
-                  )
-                } else if (plan._id != user.plan._id) {
-                  newPlanFromStripe(user, plan, status)
-                }
-
-                OkResponse()
-              }
-
-            case "customer.subscription.deleted" =>
-              // Sorry to see you go email?
-              for {
-                stripeCustomerId <- (objectJson \ "customer").extractOpt[String]
-                user <- User.find("stripeCustomerId" -> stripeCustomerId)
-                status <- (objectJson \ "status").extractOpt[String]
-                periodEnd <- (objectJson \ "current_period_end").extractOpt[Long]
-              } yield {
-                implicit val formats = User.formats
-
-                user.subscription match {
-                  case Some(subscription) =>
-                    if (status == "canceled") {
-                      User.update(
-                        ("_id" -> user._id) ~
-                        ("subscriptions._id" -> subscription._id),
-                        "$set" -> (
-                          ("subscriptions.$.status" -> "cancelled") ~
-                          ("subscriptions.$.ends" -> decompose(new DateTime(periodEnd * 1000)))
-                        )
-                      )
-                    } else { //unpaid
-                      User.update(
-                        ("_id" -> user._id) ~
-                        ("subscriptions._id" -> subscription._id),
-                        (
-                          "$set" -> (
-                            ("subscriptions.$.status" -> "stopped") ~
-                            ("subscriptions.$.ends" -> decompose(new DateTime()))
-                          ) ~
-                          (
-                            "$unset" -> ("activeCard" -> true)
-                          )
-                        )
-                      )
-                    }
-
-                  case _ =>
-                }
-
-                OkResponse()
-              }
-
-            case "customer.subscription.trial_will_end" =>
-              for {
-                stripeCustomerId <- tryo((objectJson \ "customer").extract[String]) ?~! "No customer."
-                trialEndInSecs <- tryo((objectJson \ "trial_end").extract[Long]) ?~! "No trial end."
-                user <- User.find("stripeCustomerId" -> stripeCustomerId)
-                subscription <- user.subscription
-                plan <- subscription.plan
-              } yield {
-                val trialEnd = new DateTime(trialEndInSecs * 1000)
-
-                // We get a trial end event every time a user switches plans. So, we need to distinguish
-                // between those and the real ones that come in advance of a real trial ending. We use
-                // the heuristic of subtracting one hour from the time to see if it's still in the future.
-                if (trialEnd.minusHours(1) isAfterNow)
-                  EmailActor ! SendTrialEndingEmail(user.email, user.activeCard.isDefined, plan.name, trialEnd)
-
-                OkResponse()
-              }
-
-            case _ =>
-              Full(OkResponse())
+            case "invoice.payment_succeeded" => invoicePaymentSucceeded(objectJson)
+            case "invoice.payment_failed" => invoicePaymentFailed(objectJson)
+            case "customer.subscription.created" => customerSubscriptionCreated(objectJson)
+            case "customer.subscription.updated" => customerSubscriptionUpdated(objectJson)
+            case "customer.subscription.deleted" => customerSubscriptionDeleted(objectJson)
+            case "customer.subscription.trial_will_end" => customerSubscriptionTrialWillEnd(objectJson)
+            case _ => Full(OkResponse())
           }
 
           result match {
