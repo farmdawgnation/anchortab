fd54528619e1984799814a41abd16680426b241d
Matt Farmer <matt@frmr.me>
2013-05-09 23:13:11 -0400

    Log email submission errors to NewRelic.

--- src/main/scala/com/anchortab/actor/ServiceWrapperSubmissionActor.scala
+++ src/main/scala/com/anchortab/actor/ServiceWrapperSubmissionActor.scala
@@ -1,10 +1,14 @@
 package com.anchortab.actor
 
+import scala.collection.JavaConversions._
+
 import net.liftweb._
   import common._
   import actor._
 
 import com.anchortab.model.ServiceWrapper
+
+import com.newrelic.api.agent.NewRelic
 
 case class SubscribeEmailToServiceWrapper(serviceWrapper: ServiceWrapper, email: String)
 
@@ -12,8 +16,23 @@ object ServiceWrapperSubmissionActor extends LiftActor with Loggable {
   def messageHandler = {
     case SubscribeEmailToServiceWrapper(serviceWrapper, email) =>
       serviceWrapper.subscribeEmail(email) match {
-        case Failure(msg, _, _) => logger.error("Error submitting email " + email + ":" + msg)
-        case Empty => logger.error("Got Empty while trying to submit email " + email)
+        case Failure(msg, _, _) =>
+          logger.error("Error submitting email " + email + ":" + msg)
+
+          NewRelic.noticeError("Error submitting email to external service.", Map(
+            "service wrapper" -> serviceWrapper.wrapperIdentifier,
+            "email" -> email,
+            "error message" -> msg
+          ))
+
+        case Empty =>
+          logger.error("Got Empty while trying to submit email " + email)
+
+          NewRelic.noticeError("Empty while submitting email to external service.", Map(
+            "service wrapper" -> serviceWrapper.wrapperIdentifier,
+            "email" -> email
+          ))
+
         case _ => // Do nothing.
       }
   }
