76976c635f0fb0bd9fd42d887c6f08c2c0785136
Matt Farmer <matt@frmr.me>
2013-05-11 09:55:21 -0400

    Wrap up all the createsend-java api functionality we need.

--- src/main/scala/com/anchortab/campaignmonitor/CampaignMonitor.scala
+++ src/main/scala/com/anchortab/campaignmonitor/CampaignMonitor.scala
@@ -1,12 +1,24 @@
 package com.anchortab.campaignmonitor
 
+import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
+
 import net.liftweb._
   import common._
   import util._
     import Helpers._
 
 import com.createsend._
+import com.createsend.util._
+import com.createsend.models.subscribers._
 
+case class CampaignMonitorListBasics(id: String, name: String)
+
+/**
+ * A lightweight wrapper around Campaign Monitor's createsend-java api
+ * that makes exceptions become Failures and abstracts away any nasty
+ * that would normally be involved in interacting with a Java API wrapper.
+**/
 object CampaignMonitor {
   val serviceIdentifier = "Campaign Monitor"
 
@@ -44,6 +56,57 @@ object CampaignMonitor {
       ))
     } yield {
       tokenDetails
+    }
+  }
+
+  def refreshToken(accessToken: String, refreshToken: String) = {
+    val general = new General(new OAuthAuthenticationDetails(accessToken, refreshToken))
+
+    for {
+      refreshTokenResult <- tryo(general.refreshToken)
+    } yield {
+      refreshTokenResult
+    }
+  }
+
+  def getLists(accessToken: String, refreshToken: String) = {
+    val auth = new OAuthAuthenticationDetails(accessToken, refreshToken)
+    val cmGeneral = new General(auth)
+
+    for {
+      clientIds <- tryo(cmGeneral.getClients().map(_.ClientID))
+      clients = clientIds.map(new Clients(auth, _))
+      listsBasics <- tryo(clients.map(_.lists()).flatten)
+    } yield {
+      listsBasics.map { listBasics =>
+        CampaignMonitorListBasics(listBasics.ListID, listBasics.Name)
+      }
+    }
+  }
+
+  def addSubscriber(accessToken: String, refreshToken: String, listId: String, email: String) = {
+    val auth = new OAuthAuthenticationDetails(accessToken, refreshToken)
+    val cmSubscribers = new Subscribers(auth, listId)
+
+    val subscriber = new SubscriberToAdd()
+    subscriber.EmailAddress = email
+    subscriber.ListID = listId
+
+    for {
+      result <- cmSubscribers.add(subscriber)
+    } yield {
+      result
+    }
+  }
+
+  def removeSubscriber(accessToken: String, refreshToken: String, listId: String, email: String) = {
+    val auth = new OAuthAuthenticationDetails(accessToken, refreshToken)
+    val cmSubscribers = new Subscribers(auth, listId)
+
+    for {
+      result <- tryo(cmSubscribers.unsubscribe(email))
+    } yield {
+      result
     }
   }
 }
