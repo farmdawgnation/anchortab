a05b34ae0b9c638508d78fc2b0937f7204170161
Matt Farmer <matt@frmr.me>
2013-06-05 22:46:16 -0400

    Add RecordedStripeEvent modal.

---
+++ src/main/scala/com/anchortab/model/RecordedStripeEvent.scala
@@ -0,0 +1,15 @@
+package com.anchortab.model
+
+import net.liftweb._
+  import common._
+  import mongodb._
+    import BsonDSL._
+  import util._
+    import Helpers._
+
+import org.bson.types.ObjectId
+
+case class RecordedStripeEvent(stripeEventId: String, _id: ObjectId = ObjectId.get) extends MongoDocument[RecordedStripeEvent] {
+  val meta = RecordedStripeEvent
+}
+object RecordedStripeEvent extends MongoDocumentMeta[RecordedStripeEvent]
