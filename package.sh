2d18f5b5f29281bbf46d2ab5c4070bcb78349f0f
Matt Farmer <matt@frmr.me>
2013-10-24 11:57:23 -0400

    Push fix to build process.

--- package.sh
+++ package.sh
@@ -4,6 +4,8 @@ sbt resources:compile-sass resources:copy-scripts assembly package-war
 
 echo "Adding webapp resources to JAR."
 cd target
+rm anchortab.jar
+cp scala-2.10/anchortab.jar anchortab.jar
 cp -r javascripts/* webapp/javascripts/
 zip -r anchortab.jar webapp -x webapp/WEB-INF/classes\* webapp/WEB-INF/lib\*
 cd ..
