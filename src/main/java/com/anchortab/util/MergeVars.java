a72170ddf7e900d0dd9aba1740328b5276c0c022
Matt Farmer <matt@frmr.me>
2013-10-23 22:41:45 -0400

    Start speccing out some things for MC integration.

--- src/main/java/com/anchortab/util/MergeVars.java
+++ src/main/java/com/anchortab/util/MergeVars.java
@@ -2,9 +2,20 @@ package com.anchortab.util;
 
 import com.ecwid.mailchimp.*;
 
+public class Groupings extends MailChimpObject {
+  @Field
+  public String name;
+
+  @Field
+  public String groups;
+}
+
 public class MergeVars extends MailChimpObject {
   @Field
   public String FNAME;
+
+  @Field
+  public String GROUPINGS;
 
   public MergeVars() { }
 
