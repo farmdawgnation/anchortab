875dae645427d3727fccdc75f9d28a4a3550ee39
Matt Farmer <matt@frmr.me>
2014-01-12 15:25:39 -0500

    Implement the iframe parameters for ServiceWrapper.
    When present in the service wrapper the tab is hooked up to, the success
    response from a submit will include details about a URI that the client
    should load in an hidden iframe on submit. This is ideal for service
    wrappers where we don't integrate directly with the upstream service for
    one reason or another.

--- src/main/scala/com/anchortab/model/ServiceWrapper.scala
+++ src/main/scala/com/anchortab/model/ServiceWrapper.scala
@@ -19,6 +19,12 @@ import com.ecwid.mailchimp._
 
 import org.bson.types.ObjectId
 
+case class ServiceIFrameParameters(
+  uri: String,
+  emailFieldName: String,
+  firstNameFieldName: String
+)
+
 /**
  * The ServiceWrapper trait is the common interface for all wrappers around external email
  * subscription services. The only two functions these wrappers are required to expose are
@@ -26,7 +32,9 @@ import org.bson.types.ObjectId
  * those operations are irrelevent to the parent interface.
 **/
 sealed trait ServiceWrapper {
-  def credentialsValid_? : Boolean
+  def credentialsValid_? : Boolean = true
+  def iFrameParameters: Option[ServiceIFrameParameters] = None
+
   def subscribeEmail(email: String, name: Option[String] = None) : Box[Boolean]
   def unsubscribeEmail(email:String) : Box[Boolean]
 
@@ -42,7 +50,7 @@ object ServiceWrapper {
 }
 
 case class LeadGenerationServiceWrapper(targetEmail: String) extends ServiceWrapper {
-  val credentialsValid_? = true
+  override val credentialsValid_? = true
   val wrapperIdentifier = "I don't show up on services screen."
 
   def subscribeEmail(email: String, name: Option[String] = None) = {
@@ -59,7 +67,7 @@ case class LeadGenerationServiceWrapper(targetEmail: String) extends ServiceWrap
 case class CampaignMonitorServiceWrapper(userId: ObjectId, listId: String) extends ServiceWrapper
                                                                               with CampaignMonitorCredentialsHelper
                                                                               with Loggable {
-  val credentialsValid_? = true
+  override val credentialsValid_? = true
 
   def subscribeEmail(email: String, name: Option[String] = None) = {
     val result = withAccessCredentials(userId) { (accessToken, refreshToken) =>
@@ -87,7 +95,7 @@ case class ConstantContactServiceWrapper(username:String, implicit val accessTok
   import com.anchortab.constantcontact.model.ContactLists._
 
   // This is an OAuth-based API wrapper, making the checking of valid credentials unneeded for the moment
-  val credentialsValid_? = true
+  override val credentialsValid_? = true
 
   def subscribeEmail(email:String, name: Option[String] = None) = {
     val contact = Contact(
@@ -118,7 +126,7 @@ case class ConstantContactServiceWrapper(username:String, implicit val accessTok
 }
 
 case class MailChimpServiceWrapper(apiKey:String, listId:String) extends ServiceWrapper with Loggable {
-  def credentialsValid_? = {
+  override def credentialsValid_? = {
     // Instantiate a MailChimpClient
     val mcClient = new MailChimpClient
 
