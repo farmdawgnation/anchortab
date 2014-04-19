0d1db2bf484a4cb1428d185fe3df716c5a3df3fb
Matt Farmer <matt@frmr.me>
2014-04-19 11:10:24 -0400

    Longs can also be dollars and cents?
    This is probably an issue with stripe-scala.

--- src/main/scala/com/anchortab/util/StripeNumber.scala
+++ src/main/scala/com/anchortab/util/StripeNumber.scala
@@ -15,4 +15,6 @@ case class StripeInt(input: Int) extends StripeNumber {
 
 case class StripeLong(input: Long) extends StripeNumber {
   lazy val asDateTime = new DateTime(input * 1000)
+  lazy val asDollarsAndCents = input / 100d
+  lazy val asDollarsAndCentsString = ("$%.2f" format asDollarsAndCents)
 }
