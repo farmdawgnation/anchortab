375294ba22e8e0298e5b8728e28ebcb86184efcb
Matt Farmer <matt@frmr.me>
2014-04-19 11:59:10 -0400

    D-R-Y!

--- src/main/scala/com/anchortab/snippet/Invoice.scala
+++ src/main/scala/com/anchortab/snippet/Invoice.scala
@@ -24,7 +24,7 @@ object Invoice {
   ) / "manager" / "invoice" / * >>
   TemplateBox(() => Templates("manager" :: "invoice" :: Nil))
 }
-class Invoice(invoiceId: String) {
+class Invoice(invoiceId: String) extends StripeInvoiceRendering {
   def render = {
     {
       for {
@@ -34,22 +34,7 @@ class Invoice(invoiceId: String) {
         invoice <- tryo(stripe.Invoice.retrieve(invoiceId))
          if invoice.customer == customerId
       } yield {
-        ".customer-email *" #> user.email &
-        ".paid-status *" #> (invoice.paid ? "Yes" | "No") &
-        ".invoice-date *" #> StripeNumber(invoice.date).asDateTime.toString(DATE_FORMAT) &
-        ".invoice-total *" #> StripeNumber(invoice.total).asDollarsAndCentsString &
-        ".line-item" #> invoice.lines.data.map { invoiceLine =>
-          ".line-item-description *" #> (invoiceLine.description orElse invoiceLine.plan.map(_.name)) &
-          ".period *" #> (invoiceLine.plan.isDefined ? PassThru | ClearNodes) andThen
-          ".period-start *" #> StripeNumber(invoiceLine.period.start).asDateTime.toString(DATE_FORMAT) &
-          ".period-end *" #> StripeNumber(invoiceLine.period.end).asDateTime.toString(DATE_FORMAT) &
-          ".amount *" #> StripeNumber(invoiceLine.amount).asDollarsAndCentsString
-        } &
-        ".payment-line-item" #> invoice.charge.flatMap(chargeId => tryo(stripe.Charge.retrieve(chargeId))).map { charge =>
-          ".card-type *" #> charge.card.`type` &
-          ".last-four *" #> charge.card.last4 &
-          ".amount *" #> ("-" + StripeNumber(charge.amount).asDollarsAndCentsString)
-        }
+        renderInvoice(user, invoice)
       }
     } openOr {
       "*" #> ClearNodes
